326505dd1bff1fc6248ee42b099c76ed1a14f03d
Kent English <kent.english@gmail.com>
2014-08-16 14:52:36 -0400

    Adding livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,45 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     sass = require('gulp-sass'),
+    livereload = require('gulp-livereload'),
     nodemon = require('gulp-nodemon'),
     jshint = require('gulp-jshint'),
     paths = {
         angular: [
             'bower_components/angular/angular.js',
-            'bower_components/angular/angular-route.js',
-            'bower_components/angular/angular-touch.js'
+            'bower_components/angular-route/angular-route.js',
+            'bower_components/angular-touch/angular-touch.js'
         ],
-        frontend: [
-            'src/js/app.js', 
-            'src/js/**/*.js'
-        ],
-        backend: [
-            'app.js', 
-            'controllers/**/*.js'
-        ],
+        js: {
+            backend: ['app.js', 'controllers/*.js'],
+            frontend: ['src/js/**/*.js']
+        },
         sass: [
-            'bower_components/foundation/scss/**/*.scss', 
+            'bower_components/foundation/scss/**/*.scss',
             'src/scss/*.scss'
         ],
         dest: {
             css: 'public/css',
             js: 'public/js'
-        }
+        },
+        livereload: [
+            'public/css/main.css',
+            'public/js/app.js',
+            'views/**/*.jade'
+        ]
     };
 
 gulp.task('lint', function () {
-    gulp.src(paths.backend.concat(paths.frontend))
+    gulp.src(paths.js.backend.concat(paths.js.frontend))
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('frontend', function() {
+gulp.task('js', function() {
     gulp.src(paths.angular)
         .pipe(concat('angular.js', { newLine: ';' }))
         .pipe(gulp.dest(paths.dest.js));
-    gulp.src(paths.frontend)
+    gulp.src(paths.js.frontend)
         .pipe(concat('app.js', { newLine: ';' }))
         .pipe(gulp.dest(paths.dest.js));
 });
@@ -48,9 +50,20 @@ gulp.task('sass', function () {
         .pipe(gulp.dest(paths.dest.css));
 });
 
-gulp.task('develop', function () {
-    nodemon({ script: 'app.js', ext: 'jade js scss' })
-        .on('change', ['lint', 'sass']);
+gulp.task('reload', function() {
+    setTimeout(function() {
+        livereload.changed();
+    }, 2000);
 });
 
-gulp.task('default', ['lint', 'frontend', 'sass', 'develop']);
+gulp.task('develop', function () {
+    livereload.listen();
+    nodemon({
+            script: 'app.js', 
+            ext: 'jade js scss', 
+            ignore: ['gulpfile.js', 'public/js/*.js'] 
+        })
+        .on('change', ['lint', 'js', 'sass', 'reload']);
+});
+
+gulp.task('default', ['lint', 'js', 'sass', 'develop']);
